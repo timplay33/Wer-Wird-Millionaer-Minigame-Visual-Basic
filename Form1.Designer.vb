26af3d860e5a681aa97fa2d0760bf192df94d440
Tim <tim3play3@gmail.com>
2023-02-18 01:38:09 +0100

    Added Project files

---
+++ Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Txt_question = New System.Windows.Forms.Label()
+        Me.Txt_question_id = New System.Windows.Forms.Label()
+        Me.Cmd_choice_A = New System.Windows.Forms.Button()
+        Me.Cmd_choice_B = New System.Windows.Forms.Button()
+        Me.Cmd_choice_C = New System.Windows.Forms.Button()
+        Me.Cmd_choice_D = New System.Windows.Forms.Button()
+        Me.Cmd_Next = New System.Windows.Forms.Button()
+        Me.Txt_Money = New System.Windows.Forms.Label()
+        Me.Cmd_Replay = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Txt_question
+        '
+        Me.Txt_question.Location = New System.Drawing.Point(12, 229)
+        Me.Txt_question.Name = "Txt_question"
+        Me.Txt_question.Size = New System.Drawing.Size(750, 50)
+        Me.Txt_question.TabIndex = 0
+        Me.Txt_question.Text = "question"
+        Me.Txt_question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Txt_question_id
+        '
+        Me.Txt_question_id.AutoSize = True
+        Me.Txt_question_id.Location = New System.Drawing.Point(25, 25)
+        Me.Txt_question_id.Name = "Txt_question_id"
+        Me.Txt_question_id.Size = New System.Drawing.Size(13, 15)
+        Me.Txt_question_id.TabIndex = 1
+        Me.Txt_question_id.Text = "0"
+        '
+        'Cmd_choice_A
+        '
+        Me.Cmd_choice_A.Location = New System.Drawing.Point(-7, 293)
+        Me.Cmd_choice_A.Name = "Cmd_choice_A"
+        Me.Cmd_choice_A.Size = New System.Drawing.Size(400, 50)
+        Me.Cmd_choice_A.TabIndex = 2
+        Me.Cmd_choice_A.Text = "A"
+        Me.Cmd_choice_A.UseVisualStyleBackColor = True
+        '
+        'Cmd_choice_B
+        '
+        Me.Cmd_choice_B.Location = New System.Drawing.Point(399, 293)
+        Me.Cmd_choice_B.Name = "Cmd_choice_B"
+        Me.Cmd_choice_B.Size = New System.Drawing.Size(400, 50)
+        Me.Cmd_choice_B.TabIndex = 3
+        Me.Cmd_choice_B.Text = "B"
+        Me.Cmd_choice_B.UseVisualStyleBackColor = True
+        '
+        'Cmd_choice_C
+        '
+        Me.Cmd_choice_C.Location = New System.Drawing.Point(-7, 349)
+        Me.Cmd_choice_C.Name = "Cmd_choice_C"
+        Me.Cmd_choice_C.Size = New System.Drawing.Size(400, 50)
+        Me.Cmd_choice_C.TabIndex = 4
+        Me.Cmd_choice_C.Text = "C"
+        Me.Cmd_choice_C.UseVisualStyleBackColor = True
+        '
+        'Cmd_choice_D
+        '
+        Me.Cmd_choice_D.Location = New System.Drawing.Point(399, 349)
+        Me.Cmd_choice_D.Name = "Cmd_choice_D"
+        Me.Cmd_choice_D.Size = New System.Drawing.Size(400, 50)
+        Me.Cmd_choice_D.TabIndex = 5
+        Me.Cmd_choice_D.Text = "D"
+        Me.Cmd_choice_D.UseVisualStyleBackColor = True
+        '
+        'Cmd_Next
+        '
+        Me.Cmd_Next.Location = New System.Drawing.Point(697, 426)
+        Me.Cmd_Next.Name = "Cmd_Next"
+        Me.Cmd_Next.Size = New System.Drawing.Size(75, 23)
+        Me.Cmd_Next.TabIndex = 6
+        Me.Cmd_Next.Text = "Next"
+        Me.Cmd_Next.UseVisualStyleBackColor = True
+        '
+        'Txt_Money
+        '
+        Me.Txt_Money.AutoSize = True
+        Me.Txt_Money.Location = New System.Drawing.Point(25, 201)
+        Me.Txt_Money.Name = "Txt_Money"
+        Me.Txt_Money.Size = New System.Drawing.Size(13, 15)
+        Me.Txt_Money.TabIndex = 7
+        Me.Txt_Money.Text = "0"
+        '
+        'Cmd_Replay
+        '
+        Me.Cmd_Replay.Location = New System.Drawing.Point(337, 405)
+        Me.Cmd_Replay.Name = "Cmd_Replay"
+        Me.Cmd_Replay.Size = New System.Drawing.Size(115, 23)
+        Me.Cmd_Replay.TabIndex = 8
+        Me.Cmd_Replay.Text = "Nochmal Spielen"
+        Me.Cmd_Replay.UseVisualStyleBackColor = True
+        Me.Cmd_Replay.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 461)
+        Me.Controls.Add(Me.Cmd_Replay)
+        Me.Controls.Add(Me.Txt_Money)
+        Me.Controls.Add(Me.Cmd_Next)
+        Me.Controls.Add(Me.Cmd_choice_D)
+        Me.Controls.Add(Me.Cmd_choice_C)
+        Me.Controls.Add(Me.Cmd_choice_B)
+        Me.Controls.Add(Me.Cmd_choice_A)
+        Me.Controls.Add(Me.Txt_question_id)
+        Me.Controls.Add(Me.Txt_question)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Txt_question As Label
+    Friend WithEvents Txt_question_id As Label
+    Friend WithEvents Cmd_choice_A As Button
+    Friend WithEvents Cmd_choice_B As Button
+    Friend WithEvents Cmd_choice_C As Button
+    Friend WithEvents Cmd_choice_D As Button
+    Friend WithEvents Cmd_Next As Button
+    Friend WithEvents Txt_Money As Label
+    Friend WithEvents Cmd_Replay As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+
+    Dim Question_Id As Integer
+    Dim answered As Boolean
+
+    Public Sub SetLabels(id As Integer)
+        Dim JsonData As String
+        Dim Json As Object
+
+        JsonData = My.Computer.FileSystem.ReadAllText("wwm_data.json")
+        Json = Newtonsoft.Json.Linq.JObject.Parse(JsonData)
+        Txt_question_id.Text = "Frage: " + id.ToString
+        Txt_question.Text = "Frage: " + Json.Item(id.ToString)("question").ToString
+        Txt_Money.Text = Json.Item(id.ToString)("money").ToString + "  €"
+
+        Cmd_choice_A.Text = "A: " + Json.Item(id.ToString)("answers")("A").ToString
+        Cmd_choice_B.Text = "B: " + Json.Item(id.ToString)("answers")("B").ToString
+        Cmd_choice_C.Text = "C: " + Json.Item(id.ToString)("answers")("C").ToString
+        Cmd_choice_D.Text = "D: " + Json.Item(id.ToString)("answers")("D").ToString
+
+        Cmd_choice_A.BackColor = DefaultBackColor
+        Cmd_choice_B.BackColor = DefaultBackColor
+        Cmd_choice_C.BackColor = DefaultBackColor
+        Cmd_choice_D.BackColor = DefaultBackColor
+
+    End Sub
+
+    Public Function ValidateSolution(tag As String) As Boolean
+        Dim JsonData As String
+        Dim Json As Object
+        Dim answer As String
+
+        JsonData = My.Computer.FileSystem.ReadAllText("wwm_data.json")
+        Json = Newtonsoft.Json.Linq.JObject.Parse(JsonData)
+        answer = Json.Item("aswers")(Question_Id.ToString).ToString
+        If answer = tag Then
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Question_Id = 1
+        SetLabels(Question_Id)
+
+    End Sub
+    Private Sub Cmd_choice_A_Click(sender As Object, e As EventArgs) Handles Cmd_choice_A.Click
+        If ValidateSolution("A") Then
+            Cmd_choice_A.BackColor = Color.Green
+            answered = True
+        Else
+            Cmd_choice_A.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Cmd_choice_B_Click(sender As Object, e As EventArgs) Handles Cmd_choice_B.Click
+        If ValidateSolution("B") Then
+            Cmd_choice_B.BackColor = Color.Green
+            answered = True
+        Else
+            Cmd_choice_B.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Cmd_choice_C_Click(sender As Object, e As EventArgs) Handles Cmd_choice_C.Click
+        If ValidateSolution("C") Then
+            Cmd_choice_C.BackColor = Color.Green
+            answered = True
+        Else
+            Cmd_choice_C.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Cmd_choice_D_Click(sender As Object, e As EventArgs) Handles Cmd_choice_D.Click
+        If ValidateSolution("D") Then
+            Cmd_choice_D.BackColor = Color.Green
+            answered = True
+        Else
+            Cmd_choice_D.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Cmd_Next_Click(sender As Object, e As EventArgs) Handles Cmd_Next.Click
+        If answered Then
+            If Question_Id < 15 Then
+                Question_Id = Question_Id + 1
+                SetLabels(Question_Id)
+                answered = False
+            Else
+                Txt_question.Text = "You have won"
+                Txt_question_id.Text = ""
+                Cmd_choice_A.Visible = False
+                Cmd_choice_B.Visible = False
+                Cmd_choice_C.Visible = False
+                Cmd_choice_D.Visible = False
+
+                Cmd_Next.Visible = False
+                Cmd_Replay.Visible = True
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Cmd_Replay_Click(sender As Object, e As EventArgs) Handles Cmd_Replay.Click
+        Question_Id = 1
+        SetLabels(Question_Id)
+
+        Cmd_choice_A.Visible = True
+        Cmd_choice_B.Visible = True
+        Cmd_choice_C.Visible = True
+        Cmd_choice_D.Visible = True
+
+        Cmd_Next.Visible = True
+        Cmd_Replay.Visible = False
+    End Sub
+End Class
